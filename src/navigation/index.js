caf5b069275b5c3527f2b210ba2911540175dcde
Pedro Henrique Maia <pedromaia1218@gmail.com>
2021-10-31 16:44:42 -0300

    Vector Icons

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,15 +2,15 @@ import React from "react"
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from "@react-navigation/native"
 // import Stack from "./Stack"
-// import BottomTab from './Tab'
-import Drawer from "./Drawer"
+import BottomTab from './Tab'
+// import Drawer from "./Drawer"
 
 export default props => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <NavigationContainer>
-                <Drawer />
-                {/* <BottomTab /> */}
+                {/* <Drawer /> */}
+                <BottomTab />
                 {/* <Stack /> */}
             </NavigationContainer>
         </SafeAreaView>
